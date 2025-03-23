a635d9c637304781a7926d7184e3a03a6a361ea8
Andrade <g.andrade1907@gmail.com>
2025-03-23 20:16:50 -0300

    Login clientes funcionando

--- BackEnd/database/server.js
+++ BackEnd/database/server.js
@@ -1,28 +1,72 @@
 const express = require('express');
 const cors = require('cors'); // Permite que o front acesse o backend
 const supabase = require ('./supabase.js')
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('FrontEnd'))
+
+app.post('/loginC', async(req, res) => {
+    const {email, senhaHash} = req.body;
+
+    console.log('Requisição de login recebidoa', {email, senhaHash})
+
+    try{
+        const {data: clientes, error} = await supabase
+            .from('clientes')
+            .select('senha_cliente')
+            .eq('email_cliente', email)
+
+        if (error){
+            console.error('Erro do supabase', error);
+            return res.status(500).json({error: error.message});
+        }
+        
+        if (clientes.length === 0){
+            console.log('Email ou senha incorretos');
+            return res.status(401).json({error: 'Email ou senha incorretos'});
+        }
+
+        const senhaHasBanco = clientes[0].senha_cliente // Criptogranfo a senha
+
+        const senhaCorreta = await bcrypt.compare(senhaHash, senhaHasBanco);
+
+        if (!senhaCorreta){
+            console.log('Email ou senha incorretos');
+            return res.status(401).json({error: 'Email ou senha incorretos'});
+        }
+
+        console.log('Login realizado com sucesso')
+        res.json ({message: 'Login realizado'});
+    } catch (error){
+        console.error('Erro no servidor', error)
+        res.status(500).json({error: error.message});
+    }
+});
 
 app.post('/clientes', async (req, res) => {
-    const {nome_cliente, email_cliente, telefone_cliente, senha_cliente, região_cliente} = req.body
+    const{nome_cliente, email_cliente, senha_cliente} = req.body;
 
-    // Logando os dados para ver no terminal
-    console.log("Dados recebidos do front-end:", {nome_cliente, email_cliente, telefone_cliente, senha_cliente, região_cliente});
+    try{
+        const senhaHash = await bcrypt.hash(senha_cliente, 10); 
 
-    const { data, error } = await supabase
+        const {data, error} = await supabase
         .from('clientes')
-        .insert([{nome_cliente, email_cliente, telefone_cliente, senha_cliente, região_cliente}])
+        .insert([{nome_cliente, email_cliente, senha_cliente: senhaHash}]);
 
-    if(error){
-        console.error("Erro ao salvar no banco de dados", error);
-        return res.status(500).json({message: 'Erro ao salvar no banco de dados', error})
+        if (error){
+            return res.status(500).json({error: error.message});
+        }
+
+        res.status(201).json({message: 'Cliente cadastrado com sucesso'});
+    }   catch(error){
+        res.status(500).json({error: error.message})
     }
-
-    res.status(200).json({ message: 'Cadastro realizado com sucesso!', data });
 });
+
+
 
 app.listen(3000, () =>{
     console.log('Servidor rodando na porta 3000')
--- BackEnd/database/test.js
+++
@@ -1,1 +0,0 @@
-console.log("Node.js está funcionando corretamente!");
