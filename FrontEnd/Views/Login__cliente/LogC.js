bc730f9d4c503f57fa187c36ba6bf019e1614f03
CodErick05 <141885753+CodErick05@users.noreply.github.com>
2025-05-09 20:27:49 -0300

    Add files via upload

--- FrontEnd/Views/Login__cliente/LogC.js
+++ FrontEnd/Views/Login__cliente/LogC.js
@@ -1,30 +1,31 @@
-document.querySelector(".btn").addEventListener("click", async (event) => {
-  event.preventDefault();
+document.querySelector('.btn').addEventListener('click', async(event) => {
+    event.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-  try {
-    const response = await fetch("http://localhost:3000/loginC", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: email, senha: password }),
-      redirect: "follow", // Explicitamente instrui o fetch a seguir redirecionamentos
-    });
+    try {
+        const response = await fetch ('http://localhost:3000/loginC', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: email, senhaHash: password}),
+        });
 
-    if (response.ok) {
-      console.log("Login bem-sucedido - Redirecionando no frontend...");
-      window.location.href = "/adm.html"; // Força o redirecionamento no frontend
-    } else {
-      const data = await response.json();
-      alert(`Erro no login: ${data.error}`);
-    }
-  } catch (error) {
-    {
-      console.error("Erro:", error);
-      alert("Ocorreu um erro ao tentar fazer login");
-    }
-  }
-});
+        const data = await response.json();
+
+        if (response.ok){
+            // Login bem sucedido
+            alert('Login realizado com sucesso')
+            // Redirecione para outra página, se necessário
+            // window.location.href = '/pagina-principal';
+        } else {
+            // Erro no login
+            alert(`Erro no login: ${data.error}`)
+        }
+    } catch (error){{
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao tentar fazer login')
+    }}
+})
