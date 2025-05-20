03f438c6a4bf5abc27e5034838bece31485e6dd9
CodErick05 <141885753+CodErick05@users.noreply.github.com>
2025-05-19 22:20:45 -0300

    Update home.js(updated)

--- FrontEnd/Views/Home/home.js
+++ FrontEnd/Views/Home/home.js
@@ -8,6 +8,27 @@ menuIcon.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
+
+//Menu dropdown
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdown = document.querySelector('.dropdown');
+
+    dropdownToggle.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede o comportamento padrão do link "#"
+        dropdown.classList.toggle('show');
+    });
+
+    // Fechar o dropdown se o usuário clicar fora dele
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('.dropdown-toggle') && !event.target.matches('.dropdown-menu *')) {
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show');
+            }
+        }
+    });
+});
+
 // Fecha o menu quando qualquer item de menu é clicado
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
